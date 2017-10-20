9fd11b4150cd65240d278c926418bf22cdbb5b50
juriasan <giulio.rm95@gmail.com>
2017-10-21 00:05:30 +0300

    continue developing modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { StatlistComponent } from './components/statlist/statlist.component';
+import {StatlistModule} from './components/statlist/statlist.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    StatlistComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StatlistModule
   ],
   providers: [],
   bootstrap: [AppComponent]
