83c12eda52e2dd9391d9f250b858c0b0ae438fb6
juriasan <giulio.rm95@gmail.com>
2017-10-20 14:17:32 +0300

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StatlistComponent } from './components/statlist/statlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatlistComponent
   ],
   imports: [
     BrowserModule
