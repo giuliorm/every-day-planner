3dd7aa25b897f3adca5e531fa360d716308237c7
juriasan <giulio.rm95@gmail.com>
2017-10-26 18:45:11 +0300

    Correct display on test data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {StatlistModule} from './components/statlist/statlist.module';
+import { StatlistComponent } from './components/statlist/statlist.component';
+import { StatlistItemComponent } from './components/statlist/statlist-item/statlist-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatlistComponent,
+    StatlistItemComponent
   ],
-  imports: [
-    BrowserModule,
-    StatlistModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
